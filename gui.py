7c3d1822560c1c697f4e536e07c134347c20b632
Jin Kim <jinsungkim1210@gmail.com>
2024-07-10 18:46:38 -0700

    Highlight user piece on select

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import pygame  # type: ignore
 from board import Board
 from engine import Engine
+from helpers import truncate
 
 DISPLAY_HEIGHT = 400
 DISPLAY_WIDTH = 400
@@ -50,6 +53,24 @@ class Gui:
 
   def __init__(self):
     self.game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    self.last_selected: Optional[tuple[int, int]] = None
+
+  @staticmethod
+  def round_coords(x, y) -> tuple[int, int]:
+    """
+    Takes the raw (x,y) coordinates and converts them into what the board is compatible with.
+    :return: (i, j) coords.
+    """
+    i = int(truncate(x / 50, 0))
+    j = int(truncate(y / 50, 0))
+    return i, j
+
+  def register_click(self, i, j, board: Board) -> bool:
+    if 'w' in board.board[j][i]:
+      self.last_selected = (i, j)
+      return True
+    else:
+      return False
 
   def draw_board(self):
     self.game_display.fill(GRAY_DARK)
@@ -69,11 +90,20 @@ class Gui:
         if board.board[j][i] != "--":
           self.game_display.blit(image_dict[board.board[j][i]], ((i * 50) - 5, (j * 50) - 5))
 
-  # TODO: Take user input -> Generate valid moves for that piece -> Highlight them.
-  def highlight_moves(self, engine: Engine, board: Board):
-    pass
+  def highlight_moves(self, engine: Engine):
+    if self.last_selected is not None:
+      i, j = self.last_selected
+      piece = engine.board.board[j][i]
+      valid_moves = engine.move_functions[piece[1]](i, j)
 
-  def update_game_state(self, board):
+      self.draw_square(i * 50, j * 50, YELLOW)
+
+      for move in valid_moves:
+        ci, cj = move.ni * 50, move.nj * 50
+        self.draw_square(ci, cj, BLUE_LIGHT)
+
+  def update_game_state(self, engine):
     self.draw_board()
-    self.draw_pieces(board)
+    self.highlight_moves(engine)
+    self.draw_pieces(engine.board)
     pygame.display.update()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame  # type: ignore
+
+from engine import Engine
 from gui import Gui
 from board import Board
 
@@ -8,6 +10,7 @@ if __name__ == '__main__':
   pygame.display.set_caption('Chess')
 
   board = Board()
+  engine = Engine(board)
   gui = Gui()
 
   print('Starting game.')
@@ -17,8 +20,16 @@ if __name__ == '__main__':
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
+      elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:  # Left mouse button
+          mouse_cor = pygame.mouse.get_pos()
+          mouse_x_cor = mouse_cor[0]
+          mouse_y_cor = mouse_cor[1]
+          i, j = gui.round_coords(mouse_x_cor, mouse_y_cor)
+          valid_move = gui.register_click(i, j, board)
+          print('valid_move:', valid_move)
 
-    gui.update_game_state(board=board)
+      gui.update_game_state(engine)
 
   print('Ending game.')
 
